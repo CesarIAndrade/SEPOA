b8340e6828aa59cd57b3570be56f392b5b209a1b
CesarIAndrade <42975509+CesarIAndrade@users.noreply.github.com>
2019-07-17 15:47:01 -0500

    Cambios en Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
                 <div class="content">
                     <p>Some quick example text to build on the card title and make up the bulk of the card's content.
                     </p>
-                    <a href="">Ir</a>
+                    <a href="">Ir a</a>
                 </div>
             </div>
         </div>
@@ -26,7 +26,7 @@
                 <div class="content">
                     <p>Some quick example text to build on the card title and make up the bulk of the card's content.
                     </p>
-                    <a href="">Ir</a>
+                    <a href="">Ir a</a>
                 </div>
             </div>
         </div>
@@ -40,7 +40,7 @@
                 <div class="content">
                     <p>Some quick example text to build on the card title and make up the bulk of the card's content.
                     </p>
-                    <a href="">Ir</a>
+                    <a href="">Ir a</a>
                 </div>
             </div>
         </div>
@@ -54,7 +54,7 @@
                 <div class="content">
                     <p>Some quick example text to build on the card title and make up the bulk of the card's content.
                     </p>
-                    <a href="">Ir</a>
+                    <a href="">Ir a</a>
                 </div>
             </div>
         </div>
